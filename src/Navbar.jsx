0d54d672065a6dffa2121795d7cbc95823900368
Tushar garg <tushargarg50797@gmail.com>
2025-08-18 14:38:10 +0530

    updated meta tags and logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -20,8 +20,8 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           
-          {/* Logo */}
-          <div className="flex-shrink-0 text-green-600 text-xl font-bold">
+          <div className="flex-shrink-0 text-green-600 text-xl font-bold flex gap-1">
+          <img className="h-7 w-7 "  src="../pill.png" alt="" />
             CureConnect
           </div>
 
